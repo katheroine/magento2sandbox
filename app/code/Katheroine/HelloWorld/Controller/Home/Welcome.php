40b369b6728c5f0067daca714b23455cfdb436fa
Katarzyna Krasinska <katarzyna.krasinska@accenture.com>
2019-06-06 14:50:19 +0200

    Module simple controller action created to be completed with view later (#5)

---
+++ app/code/Katheroine/HelloWorld/Controller/Home/Welcome.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Katheroine\HelloWorld\Controller\Home;
+
+use Magento\Framework\App\Action\Action;
+
+class Welcome extends Action
+{
+    public function execute()
+    {
+        echo "Under construction";
+    }
+}
