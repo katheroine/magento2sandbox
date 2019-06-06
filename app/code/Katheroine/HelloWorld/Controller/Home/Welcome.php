809667295720857f9229b2fffbd452d37a852c26
Katarzyna Krasinska <katarzyna.krasinska@accenture.com>
2019-06-06 16:10:44 +0200

    Module with tiny view created (#5)

--- app/code/Katheroine/HelloWorld/Controller/Home/Welcome.php
+++ app/code/Katheroine/HelloWorld/Controller/Home/Welcome.php
@@ -3,11 +3,37 @@
 namespace Katheroine\HelloWorld\Controller\Home;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\Page;
 
 class Welcome extends Action
 {
+    /**
+     * @var PageFactory
+     */
+    private $pageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    ) {
+        $this->pageFactory = $pageFactory;
+
+        parent::__construct($context);
+    }
+
+    /**
+     * @return ResponseInterface|ResultInterface|Page
+     */
     public function execute()
     {
-        echo "Under construction";
+        return $this->pageFactory->create();
     }
 }
