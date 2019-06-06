cf2a9c0ecc6964c691f346828e71c6a88c7b93d0
Katarzyna Krasinska <katarzyna.krasinska@accenture.com>
2019-06-06 10:16:26 +0200

    Module with simple controller action created (#3)

---
+++ app/code/Katheroine/Simplicity/Controller/Excelsis/Gloria.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Katheroine\Simplicity\Controller\Excelsis;
+
+use Magento\Framework\App\Action\Action;
+
+class Gloria extends Action
+{
+    public function execute()
+    {
+        echo 'Gloria in excelsis!';
+    }
+}
