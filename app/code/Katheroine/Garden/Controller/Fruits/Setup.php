57c11b0e979696451e99d4e5167eeb616ec9f2ed
Katarzyna Krasinska <katarzyna.krasinska@accenture.com>
2019-06-07 12:55:18 +0200

    Route defined & empty action created (#7)

---
+++ app/code/Katheroine/Garden/Controller/Fruits/Setup.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Katheroine\Garden\Controller\Fruits;
+
+use Magento\Framework\App\Action\Action;
+
+class Setup extends Action
+{
+    public function execute()
+    {
+        echo 'To be continued...';
+    }
+}
